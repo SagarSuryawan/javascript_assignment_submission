4890c76976f19d8c74609424e0420e0674751745
SagarSuryawan <sagarsurya45@gmail.com>
2023-07-02 16:40:58 +0530

    calculator app done

--- milestone_exam/problem_1/index.js
+++ milestone_exam/problem_1/index.js
@@ -1,3 +1,9 @@
+
+// Write a JavaScript program that checks if the entered password matches the confirmed password. If the
+// passwords match, the program should log "Password Matched. Password validation Successful." to the console.
+// Otherwise, it should log "Password didn't match. Password validation unsuccessful" to the consoleQ
+
+
 let btn = document.getElementById("check")
 
 btn.addEventListener("click", () =>{
